850df579cba886b8cb1d150730c3eb6adf0bc11b
brunoguyy12 <130707540+brunoguyy12@users.noreply.github.com>
2024-06-04 12:35:33 +0530

    Added another Item

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url="https://tomato-food-delivery-sqsz.onrender.com";
 
   return (
     <div>
